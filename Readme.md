5fae80c88393c18033eb5522f381f93af4eabb63
Tedy Fernando <teddyfernando45@gmail.com>
2024-02-28 13:47:01 +0700

    router mount

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 1. module init : go mod init github.com/tedyfd/go-restapi
 2. build to binary files : go build .
 3. running : go run .
-4.
+4. get module : go get github.com/joho/godotenv
+5. download module : go mod vendor
--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
+
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+
 	"github.com/joho/godotenv"
 )
 
-func main(){
-	fmt.Println("hello")
+func main() {
 
-	godotenv.Load(".env")
+	godotenv.Load()
 
 	portString := os.Getenv("PORT")
-	if portString == ""{
+	if portString == "" {
 		log.Fatal("Port not found")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins: []string{"http://localhost"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.HandleFunc("/ready", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server Run on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 	fmt.Println("Port:", portString)
 
